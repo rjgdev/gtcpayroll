3a1bb8f004259837c676ac0fb8411878bca69160
jamiecapistrano <capistrano.jamiee@gmail.com>
2019-07-23 08:50:01 +0800

    Toast and Department

--- application/controllers/Department.php
+++ application/controllers/Department.php
@@ -9,7 +9,45 @@ class Department extends CI_Controller {
 			'title' => 'Department'
 			);
 		$this->load->view('Template/Header', $data);
+		$query = $this->db->get("department");
+		$data ['records'] = $query->result();
 		$this->load->view('Settings/department', $data);			
 		$this->load->view('Template/Footer',  $data);
 	}
+	public function department_action(){  
+	           if($_POST["action"] == "Add")  
+	           {  
+	                $data = array(  
+	                    'description' => $this->input->post('description')
+	                );  
+	                $this->load->model('Department_model');  
+	                $this->Department_model->adddepartment($data); 
+	                $this->session->set_flashdata('department', 'success'); 
+	                redirect("Department/index");
+
+	           }  
+	           if($_POST["action"] == "Edit")  
+	           {   
+	                $updated_data = array(  
+	                    'description' => $this->input->post('description')
+	                );  
+	                $this->load->model('Department_model');  
+	                $this->Department_model->update($this->input->post("departmentID"), $updated_data); 
+	                $this->session->set_flashdata('department', 'success'); 
+	                redirect("Department"); 
+	           }  
+	      }
+
+	    public function fetch_single_user()  
+	      {  
+	           $output = array();  
+	           $this->load->model("Department_model");  
+	           $data = $this->Department_model->fetch_single_user($_POST["departmentID"]);  
+	           foreach($data as $r)  
+	           {  
+	                $output['description'] = $r->description; 
+	           }  
+	           echo json_encode($output);  
+	      }    
 }
+?>
---
+++ application/views/Settings/department.php
@@ -0,0 +1,92 @@
+<script>
+  $(function () {
+    $('.select2').select2();
+  });
+</script>
+<div class="main-panel">
+	<div class="content-wrapper">
+	    <nav aria-label="breadcrumb">
+	      <ol class="breadcrumb breadcrumb-custom bg-inverse-primary">
+	        <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-view-dashboard"></i> Dashboard</a></li>
+	        <li class="breadcrumb-item active" aria-current="page"><span><i class="mdi mdi-account-multiple-outline"></i>Department</span></li>
+	      </ol>
+	    </nav>
+    <div class="row">
+    <div class="col-md-4 grid-margin stretch-card">
+    </div>
+    <div class="col-md-4 grid-margin stretch-card">
+        <div class="card">
+      <div class="card-body test-card">
+        
+          <div class="d-flex align-items-center justify-content-between flex-wrap border-bottom pb-3 mb-3">
+            <div class="d-flex align-items-center">
+              <h6 class="mb-0 font-weight-bold"><i class="mdi mdi-account-multiple-outline"></i>Set Up Department</h6>
+            </div>
+            <div class="mt-3 mt-md-0">
+              <button class="btn btn-primary btn-rounded btn-sm" id="add_button" data-toggle="modal" data-target="#departmentModal"><i class="mdi mdi-account-plus "></i> New Department</button>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="table-responsive">
+              <table id="order-listing" class="table">
+                <span><?php if($this->session->flashdata('department')=="success") echo '<script type="text/javascript"> showSuccessToast() </script>';?></span>
+                <thead>
+                  <tr>
+                      <th>Leave ID</th>
+                      <th>Type of Department</th>
+                  </tr>
+                </thead>
+                <tbody id="showdata">
+                  <?php
+                        foreach ($records as $r) {
+
+                          echo '<tr>';
+                          echo '<td>'.$r->departmentID.'</td>'; 
+                          echo '<td>'.$r->description.'</td>';
+                          echo '<td><button type="button" name="update" id="'.$r->departmentID.'" class="btn btn-outline-primary item-edit" data-target="#departmentModal">View</button></td>' ;
+                        }
+                      ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+    </div>
+	</div>
+	</div>
+</div>
+
+<div class="modal fade" id="departmentModal" role="dialog" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+    
+      <!-- Modal content-->
+      <form id="commentForm" method="post" action="<?php echo site_url('Department/department_action'); ?>">
+      <div class="modal-content">
+        <div class="modal-header" style="background-color: #f6f7fb;">
+          <h3 class="modal-title" id="ModalLabel">Set up Department</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;
+          </button>
+        </div>
+        <fieldset>
+        <div class="modal-body">
+          <div class="row grid-margin">
+          <div class="col-lg-12">
+
+          <div class="form-group">
+            <div class="row">
+            <div class="col">
+              <label for="description">Type of Department</label>
+              <input id="description" type="text" name="description" class="form-control"  required>
+            </div>
+          </div>
+    
+            <div class="modal-footer">
+              <input type="hidden" name="departmentID" id="departmentID" />  
+              <input type="submit" name="action" id="action" class="btn btn-primary" value="Add" />   
+          </div>
+          </fieldset>
+        </div>
+      </form>
+    </div>
+</div>
